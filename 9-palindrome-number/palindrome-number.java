c967f0898f2d5d9a31589b3bc17e730c4ebbe4b1
Tirtha Sharma <tirthagenze121@gmail.com>
2024-12-30 23:55:00 +0530

    Time: 4 ms (100.00%) | Memory: 44 MB (64.82%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -5,11 +5,10 @@ class Solution {
         int palindrome = 0;
         while(temp!=0){
             int remainder = temp%10;
-            if(palindrome > Integer.MAX_VALUE/10 || palindrome < Integer.MIN_VALUE/10) return false;
             palindrome = palindrome * 10 + remainder;
             temp = temp/10;
         }
 
-        return (palindrome == x);
+        return (palindrome == x) ? true : false;
     }
 }
