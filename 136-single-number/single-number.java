bfb445e3f99341ac2dd8d602cdffa24f5f8be749
Tirtha Sharma <tirthagenze121@gmail.com>
2025-05-24 17:27:59 +0530

    Time: 1 ms (99.93%) | Memory: 46.4 MB (36.09%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int singleNumber = 0;
+        for(int num:nums){
+            singleNumber = singleNumber ^ num;
+        }
+        return singleNumber;
+    }
+}
