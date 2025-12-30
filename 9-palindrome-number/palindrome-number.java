3e2c046369aa032dc3c6c95a832857732016ac36
Tirtha Sharma <tirthagenze121@gmail.com>
2025-12-30 23:19:55 +0530

    Time: 5 ms (73.94%) | Memory: 46.1 MB (30.24%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -3,12 +3,12 @@ class Solution {
         if(x < 0) return false;
         int temp = x;
         int palindrome = 0;
-        while(temp!=0){
-            int remainder = temp%10;
-            palindrome = palindrome * 10 + remainder;
-            temp = temp/10;
+        while(temp > 0){
+            int remainder = temp % 10;
+            if(palindrome > Integer.MAX_VALUE/10 || palindrome < Integer.MIN_VALUE/10) return false;
+            palindrome = palindrome * 10 +  remainder;
+            temp = temp / 10;
         }
-
-        return (palindrome == x) ? true : false;
+        return (palindrome == x);
     }
 }
