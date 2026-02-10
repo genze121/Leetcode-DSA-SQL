d19c243d1d567565374f4620552d593b14b58020
Tirtha Sharma <tirthagenze121@gmail.com>
2026-02-10 19:16:09 +0530

    Time: 1 ms (99.64%) | Memory: 46.9 MB (65.10%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,31 +1,18 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-
-        // Approach - 1
-        List<String> list = new ArrayList<>();
+        List<String> fizzBuzzList = new ArrayList<String>();
         for(int i=1;i<=n;i++){
             if(i%3==0 && i%5==0){
-                list.add("FizzBuzz");
+                fizzBuzzList.add("FizzBuzz");
             }else if(i%3==0){
-                list.add("Fizz");
+                fizzBuzzList.add("Fizz");
             }else if(i%5==0){
-                list.add("Buzz");
+                fizzBuzzList.add("Buzz");
             }else{
-               list.add(String.valueOf(i));
+                fizzBuzzList.add(String.valueOf(i));
             }
         }
 
-        return list;
-
-        //  // Approach - 2
-        for(int i=1;i<=n;i++){
-            list.add(
-                i%3==0 && i%5==0 ? "FizzBuzz" :
-                i%3==0 ? "Fizz" :
-                i%5==0 ? "Buzz" : String.valueOf(i)
-            );
-        }
-
-        return list;
+        return fizzBuzzList;
     }
 }
