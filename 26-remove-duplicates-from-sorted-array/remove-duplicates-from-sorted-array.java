20630186fe7e2c70d989df58df220ea873a11a1d
Tirtha Sharma <tirthagenze121@gmail.com>
2026-04-13 21:02:32 +0530

    Time: 1 ms (77.09%) | Memory: 46.6 MB (84.74%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,32 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // Approach - 1 ==> Brute Force / Naive Approach => Time Complexity = O(n), Space Complexity = O(n)
-        int n = nums.length;
-        int[] temp = new int[n]; // extra space
-        temp[0] = nums[0];
-        int result = 1;
-        for(int i=1;i<n;i++){
-            if(temp[result-1]!=nums[i]){
-                temp[result] = nums[i];
-                result++;
-            }
-        }
-
-        for(int i=0;i<result;i++){
-            nums[i] = temp[i];
-        }   
-
-        return result;
-
-        // Approach - 2 ==> Efficient Approach => Time Complexity = O(n), Space Complexity = O(1)
-        int result = 1;
+        int leftPointer = 0;
+        int rightPointer = 1;
         for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[result-1]){
-                nums[result] = nums[i];
-                result++;
+            if(nums[i] != nums[i-1]){
+                nums[rightPointer] = nums[i];
+                rightPointer++;
             }
         }
 
-        return result;
+        return rightPointer;
     }
 }
